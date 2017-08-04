5502ec4b0ba8c9be3b871a84a18c195dc4737771
v-akshsh <v-akshsh@01HW455142>
2017-08-04 15:30:08 +0530

    Add project files.

---
+++ MVC Test/DAL/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using MVC_Test.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MVC_Test.DAL
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
